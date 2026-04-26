400b0cd88061bdcd1a8709960eeeafcae51f4497
Michael Paonam <mazdaswift@gmail.com>
2026-04-26 12:44:11 +0530

    level 2 - added crates

--- main/hook/cable.go
+++ main/hook/cable.go
@@ -2,3 +2,13 @@ components {
   id: "cable_line"
   component: "/main/hook/cable_line.script"
 }
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"default\"\n"
+  "material: \"/main/hook/cable.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/hook/cable.atlas\"\n"
+  "}\n"
+}
